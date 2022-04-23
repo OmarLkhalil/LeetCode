0a96f6e774fdfce2e45d7e92ea205dfeb6e4d1d4
Omar Mohamed <98606902+OmarLkhalil@users.noreply.github.com>
2022-04-23 06:55:19 +0200

    Create Solution.java

---
+++ 9. Palindrome Number/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+	
+    int number = x;
+	int reverse = 0;
+	while (x != 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+        return reverse == number;
+    }}
